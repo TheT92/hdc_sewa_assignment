871f7610b9718c8b0364e734573732af421ce70f
597367462@qq.com <597367462@qq.com>
2024-04-15 14:52:12 +0100

    icon replacement

--- index.php
+++ index.php
@@ -31,11 +31,10 @@
                     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
                   </svg>';
                 } else {
-                    echo '<svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="currentColor" class="bi bi-person"
-                    viewBox="0 0 16 16">
-                    <path
-                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
-                    </svg>';
+                    echo '<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
+                    <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+                  </svg>';
                 }
                 ?>
 
@@ -157,12 +156,12 @@
                 SPECIAL OFFERS
             </h3>
             <section class="row gx-3 gy-3">
-                <section class="col-12 col-sm-4 col-md-3">
+                <section class="col-12 col-sm-12 col-md-3">
                     <section class="special-item bg-color-primary">
                         <span class="d-block img" style="background-image: url('./images/img1.jpg');"></span>
                         <section class="p-2">
-                            <h5>Camping</h5>
-                            <span class="desc overflow-hidden">2 days and 1 night of wild camping, we provide tents and
+                            <h5 class="mb-1 fw-bold">Camping</h5>
+                            <span class="desc lh-sm overflow-hidden">2 days and 1 night of wild camping, we provide tents and
                                 other basic equipments.</span>
                         </section>
                     </section>
@@ -171,22 +170,22 @@
                 <!-- Force next columns to break to new line -->
                 <!-- <section class="w-100"></section> -->
 
-                <section class="col-12 col-sm-4 col-md-3">
+                <section class="col-12 col-sm-12 col-md-3">
                     <section class="special-item bg-color-primary">
                         <span class="d-block img" style="background-image: url('./images/img9.jpg');"></span>
                         <section class="p-2">
-                            <h5>Camping</h5>
-                            <span class="desc overflow-hidden">2 days and 1 night of wild camping, we provide tents and
+                            <h5 class="mb-1 fw-bold">Boating</h5>
+                            <span class="desc lh-sm overflow-hidden">2 days and 1 night of wild camping, we provide tents and
                                 other basic equipments.</span>
                         </section>
                     </section>
                 </section>
-                <section class="col-12 col-sm-4 col-md-3">
+                <section class="col-12 col-sm-12 col-md-3">
                     <section class="special-item bg-color-primary">
                         <span class="d-block img" style="background-image: url('./images/img8.jpg');"></span>
                         <section class="p-2">
-                            <h5>Camping</h5>
-                            <span class="desc overflow-hidden">2 days and 1 night of wild camping, we provide tents and
+                            <h5 class="mb-1 fw-bold">Camping</h5>
+                            <span class="desc lh-sm overflow-hidden">2 days and 1 night of wild camping, we provide tents and
                                 other basic equipments.</span>
                         </section>
                     </section>
--- register.php
+++ register.php
@@ -81,10 +81,12 @@
         </a>
         <span>
             <a href="login.php" class="p-2 ms-3">
-                <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="currentColor" class="bi bi-person"
-                    viewBox="0 0 16 16">
-                    <path
-                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor"
+                    class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z" />
+                    <path fill-rule="evenodd"
+                        d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
                 </svg>
             </a>
         </span>
@@ -97,7 +99,7 @@
             <section class="mb-2">
                 <label for="email" class="form-label">Email address</label>
                 <input type="email" id="email" name="email" class="form-control" aria-describedby="emailHelp">
-                <span id="emailHelp" class="form-text">We'll never share your email with anyone else.</span><br/>
+                <span id="emailHelp" class="form-text">We'll never share your email with anyone else.</span><br />
                 <span class="error text-danger"><?php echo $emailErr; ?></span>
             </section>
             <section class="mb-2">
