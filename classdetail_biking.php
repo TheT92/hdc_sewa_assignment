7a34b353330c3a537b8aa2fd90b7466e05d6b998
xinmei540 <736560054@qq.com>
2024-04-30 21:16:06 +0800

    class detail

---
+++ classdetail_biking.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/app.css" />
+    <!-- <link rel="stylesheet" href="css/index.css" /> -->
+    <link rel="stylesheet" href="css/classdetail.css" />
+    <title>Biking</title>
+    </head>
+
+    <body id="index" class="page-container">
+        <?php include './app/views/header.php' ?>
+        <section class="image">
+            <img src = "./images/biking/1.jpg" alt="..." >
+        </section>
+
+        <!-- <section class="animation">
+            <ul class="iamge">
+                <li class="active"><img src="./images/biking/1.jpg"/></li>
+                <li class="active"><img src="./images/biking/2.jpg"/></li>
+                <li class="active"><img src="./images/biking/3.jpg"/></li>
+                <li class="active"><img src="./images/biking/4.jpg"/></li>
+                <li class="active"><img src="./images/biking/5.jpg"/></li>
+            </ul>
+        </section> -->
+
+    </body>
+</html>
