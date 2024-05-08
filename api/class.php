4b45635b64f1c186faaaeee187f04ee0a479c5fc
597367462@qq.com <597367462@qq.com>
2024-05-08 17:54:21 +0100

    rebase main

--- api/class.php
+++ api/class.php
@@ -3,6 +3,6 @@ include("databaseVariables.php");
 $conn = new PDO("mysql:host=$servername", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$sql = "SELECT `id`,`class_name`,`class_intro`,`class_time`,`member_level` FROM sewagroup.class";
+$sql = "SELECT `id`,`class_name`,`class_intro`,`class_time`,`member_level`, `class_cover_img` FROM sewagroup.class";
 $classList = $conn->query($sql);
 return $classList;
